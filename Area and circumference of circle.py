6386e6202cf994301e5ae62454987a38cc4328f0
HackerSan72 <hackersan72@gmail.com>
2026-07-23 13:43:42 +0530

    Solution to problem 1

---
+++ Area and circumference of circle.py
@@ -0,0 +1,6 @@
+#1) Input the radius of a circle and calculate the area and circumference of circle
+
+r = int(input("Enter the radius of circle: "))
+area = (22/7) * r * r
+circumference = 2 * (22/7) * r
+print(area, circumference)
