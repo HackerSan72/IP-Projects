7b2bfef37a92e9867807c812295d0af4c21291d0
HackerSan72 <hackersan72@gmail.com>
2026-07-27 17:20:48 +0530

    Solution to problem 7

---
+++ Check_whether_no._is_even_or_odd.py
@@ -0,0 +1,8 @@
+#7) Input a number and find whether the entered number is even or odd. 
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("This number is even")
+else:
+    print("This number is odd")
