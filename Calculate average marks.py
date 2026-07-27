477dae0a4223720793b3c4953f27c7fa621030b0
HackerSan72 <hackersan72@gmail.com>
2026-07-27 16:58:22 +0530

    Solution to problem 3

---
+++ Calculate average marks.py
@@ -0,0 +1,9 @@
+#3) Input the marks of 5 subjects, calculate the average marks.
+p = int(input("Enter marks of physics out of 100:"))
+c = int(input("Enter marks of chemistry out of 100:"))
+m = int(input("Enter marks of math out of 100:"))
+e = int(input("Enter marks of english out of 100:"))
+ip = int(input("Enter marks of IP out of 100:"))
+
+Avg = (p+c+m+e+ip)/5
+print("The average is: ", Avg)
