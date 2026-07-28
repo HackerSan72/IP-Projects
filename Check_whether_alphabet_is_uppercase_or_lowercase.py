00426e494b8de79eb37a4346ab02c491d429a946
HackerSan72 <hackersan72@gmail.com>
2026-07-28 21:51:55 +0530

    Fixed mistakes in problem 9

--- Check_whether_alphabet_is_uppercase_or_lowercase.py
+++ Check_whether_alphabet_is_uppercase_or_lowercase.py
@@ -1,8 +1,8 @@
 #9) Input an alphabet and find whether it is in uppercase or lowercase
-a = int(input("Enter an alphabet: "))
-if a == 'ABCDEFGHIJKLMNOPQRSTUVwXYZ':
+a = input("Enter an alphabet: ")
+if a in 'ABCDEFGHIJKLMNOPQRSTUVwXYZ':
     print(a ,"is an uppercase letter")
-elif a == 'abcdefghijklmnopqrstuvwxyz':
+elif a in 'abcdefghijklmnopqrstuvwxyz':
     print(a,"is an lowercase letter")
 else:
     print("Unexpected input!")
---
+++ Check_whether_alphabet_is_vowel_or_consonant.py
@@ -0,0 +1,3 @@
+#10) Input an alphabet and find whether it is a vowel or consonant. 
+
+a
