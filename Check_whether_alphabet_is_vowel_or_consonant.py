d46368524d478bba186a248c019762cf02b505fe
HackerSan72 <hackersan72@gmail.com>
2026-07-28 21:56:32 +0530

    Solution to problem 10

--- Check_whether_alphabet_is_vowel_or_consonant.py
+++ Check_whether_alphabet_is_vowel_or_consonant.py
@@ -1,3 +1,10 @@
 #10) Input an alphabet and find whether it is a vowel or consonant. 
 
-a
+a = input("Enter an alphabet: ")
+if a in "AEIOU" or a in "aeiou":
+    print(a,"is vowel")
+elif a in "BCDFGHJKLMNPQRSTVWXYZ" or a in "bcdfghjklmnpqrstuvwxyz":
+    print(a,"is a consonent")
+else:
+    print("Unexpected input!")
+    
